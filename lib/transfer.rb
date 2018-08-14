98511ba79f8ec7d195c5a0c152cc7c495e3c2cbc
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-14 06:11:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && self.status != "complete" && @sender.balance >= @amount
+    if self.valid? && self.status != "complete" && sender.balance >= @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       self.status = complete
