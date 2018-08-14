212a4c07ca04b00969a97c145c0771b8e32807b9
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-14 06:16:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,9 @@ class Transfer
         @sender.balance -= @amount
         @receiver.balance += @amount
         self.status = "complete"
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
     end
   end
 end
